d930359a20f44d6e0598ee631af98fb18e6484aa
Yusuke Yamada <yamachu.dev@gmail.com>
2017-09-11 21:40:58 +0900

    Mac deploy fix

--- tools/deploy_mac.sh
+++ tools/deploy_mac.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-type dotnet >/dev/null 2>&1
-DOTNET_EXIST=$?
+# type dotnet >/dev/null 2>&1
+# DOTNET_EXIST=$?
 
-if [[ $DOTNET_EXIST != 0 ]]; then
-    echo 'This project required dotnet core'
-    echo 'https://www.microsoft.com/net/core'
-    exit 1
-fi
+# if [[ $DOTNET_EXIST != 0 ]]; then
+#     echo 'This project required dotnet core'
+#     echo 'https://www.microsoft.com/net/core'
+#     exit 1
+# fi
 
 dotnet restore src/cs
 dotnet publish src/cs --runtime osx.10.12-x64 --configuration Release
